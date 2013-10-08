881e0350713e51f7492978e4fb463fde4fc6c915
Jonathan McPherson <jonathan@rstudio.com>
2013-10-08 16:44:46 -0700

    Write simple DESCRIPTION file with app's dependencies

--- R/packrat.R
+++ R/packrat.R
@@ -1,4 +1,12 @@
-bootstrap <- function() {
+#' Given an application directory, computes the application's dependencies, 
+#' and places the application's dependencies under packrat control. 
+#' 
+#' @export
+bootstrap <- function(appDir = getwd()) {
+  dependencies <- data.frame(Source = getOption("repos")[[1]],
+                             Dependencies = paste(appDependencies(appDir),
+                                                  collapse=", "))
+  write.dcf(dependencies, file = paste(appDir, "/DESCRIPTION", sep = ""))
 }
 
 install <- function() {
