d6d9bea04ec078454192060954c9eac7417359ae
Joe Cheng <joe@rstudio.com>
2013-10-09 14:26:53 -0700

    Create/augment .Rprofile and .Renviron to use private library

--- R/packrat.R
+++ R/packrat.R
@@ -40,3 +40,61 @@ install <- function(appDir = getwd()) {
 
 pack <- function() {
 }
+
+#' Install .Rprofile and .Renviron files in the given directory to make it
+#' use a private package library.
+#' 
+#' @export
+packify <- function(dir = '.') {
+  dir <- normalizePath(dir, TRUE)
+  rprofile <- file.path(dir, '.Rprofile')
+  renviron <- file.path(dir, '.Renviron')
+  
+  augmentFile(system.file('Rprofile', package='packrat'), rprofile, TRUE)
+  augmentFile(system.file('Renviron', package='packrat'), renviron, FALSE)
+}
+
+#' Add the contents of srcFile into targetFile, with "magic" comments bracketing
+#' the contents. Should be safe to run multiple times--each subsequent call will
+#' replace the contents between magic comments.
+#' 
+#' @keywords internal
+augmentFile <- function(srcFile, targetFile, preferTop) {
+  header <- '# -- BEGIN PACKRAT --\n'
+  footer <- '# -- END PACKRAT --'
+  headerFooterRegex <- '# -- BEGIN PACKRAT --\\s*\n.*?# -- END PACKRAT --'
+  emptyHeaderFooter <- paste(header, footer, sep='')
+  
+  src <- paste(readLines(srcFile, warn=FALSE), collapse='\n')
+  target <- if (file.exists(targetFile)) {
+    paste(readLines(targetFile, warn=FALSE), collapse='\n')
+  } else {
+    ''
+  }
+  
+  target <- gsub(headerFooterRegex,
+                 emptyHeaderFooter,
+                 target)
+  
+  if (!isTRUE(grepl(paste(header, footer, sep=''), target, fixed = TRUE))) {
+    if (preferTop) {
+      if (nzchar(target) > 0)
+        target <- paste(emptyHeaderFooter, target, sep='\n')
+      else
+        target <- emptyHeaderFooter
+    } else {
+      if (nzchar(target) > 0)
+        target <- paste(target, emptyHeaderFooter, sep='\n')
+      else
+        target <- emptyHeaderFooter
+    }
+  }
+  
+  target <- gsub(headerFooterRegex,
+                 paste(header, src, '\n', footer, sep=''),
+                 target)
+
+  writeLines(target[[1]], targetFile)
+  
+  invisible()
+}
