c7ca39e6f61de9f334862ead87d0013c4ac9ddab
Joe Cheng <joe@rstudio.org>
2013-10-16 14:00:34 -0700

    Append InstallAgent field to DESCRIPTION files in binary packages we install

--- R/descfile.R
+++ R/descfile.R
@@ -43,3 +43,18 @@ addDependencies <- function(appDir, packages) {
   desc$Depends <- paste(depends, collapse=', ')
   setDescription(appDir, desc)
 }
+
+# check whether the specified file ends with newline
+ends_with_newline <- function(path) {
+  conn <- file(path, open = "rb", raw = TRUE)
+  on.exit(close(conn))
+  seek(conn, where = -1, origin = "end")
+  lastByte <- readBin(conn, "raw", n = 1)
+  lastByte == 0x0a
+}
+
+appendToDcf <- function(path, records) {
+  if (!ends_with_newline(path))
+    cat('\n', sep='', file=path, append=TRUE)
+  write.dcf(records, path, append = TRUE)
+}
