1dc3393b89d0036332a5fd84447ac310913b5460
Joe Cheng <joe@rstudio.org>
2013-10-28 12:59:49 -0700

    Roxygenize existing docs

--- R/packrat.R
+++ R/packrat.R
@@ -1,18 +1,113 @@
+#' Packrat: Reproducible dependency management
+#' 
+#' Packrat is a tool for managing the R packages your project depends on in 
+#' an isolated, portable, and reproducible way.
+#' 
+#' Use packrat to make your R projects more:
+#' 
+#' \itemize{
+#' \item \strong{Isolated}: Installing a new or updated package for one project
+#' won't break your other projects, and vice versa. That's because packrat gives
+#' each project its own private package library.
+#' \item \strong{Portable}: Easily transport your projects from one computer to
+#' another, even across different platforms. Packrat makes it easy to install the
+#' packages your project depends on.
+#' \item \strong{Reproducible}: Packrat records the exact package versions you
+#' depend on, and ensures those exact versions are the ones that get installed
+#' wherever you go.}
+#' 
+#' Use \code{\link{bootstrap}} to create a new packrat project,
+#' \code{\link{snapshot}} to record changes to your project's library, and
+#' \code{\link{restore}} to recreate your library the way it was the last time you
+#' (or anyone!) took a snapshot. 
+#' 
+#' Using these simple functions and sharing packrat's files lets you collaborate
+#' in a shared, consistent environment with others as your project grows and
+#' changes, and provides an easy way to share your results when you're done. 
+#' 
+#' @section Anatomy of a packrat project:
+#' 
+#' A packrat project contains a few extra files and directories. The
+#' \code{\link{bootstrap}} function creates these files for you, if they don't
+#' already exist.
+#' 
+#' \describe{
+#'   \item{\code{library/}}{Private package library for this project.}
+#' 
+#'   \item{\code{packrat.lock}}{Lists the precise package versions that were used
+#' to satisfy dependencies, including dependencies of dependencies. (This file
+#' should never be edited by hand!)}
+#' 
+#'   \item{\code{packrat.sources/}}{Source packages of all the dependencies that
+#' packrat has been made aware of.}
+#' 
+#'   \item{\code{.Rprofile and .Renviron}}{Directs R to use the private package
+#' library (when it is started from the project directory).}
+#' }
+#' 
+#' @section Using packrat with version control:
+#' 
+#' Packrat is designed to work hand in hand with Git, Subversion, or any other
+#' version control system. Be sure to check in the \code{.Rprofile},
+#' \code{.Renviron}, and \code{packrat.lock} files, as well as everything under
+#' \code{packrat.sources/}. You can tell your VCS to ignore \code{library} (or
+#' feel free to check it in if you don't mind taking up some extra space in your
+#' repository).
+#' 
+#' @examples
+#' \dontrun{
+#' # Create a new packrat project from an existing directory of R code
+#' bootstrap()
+#' 
+#' # Install a package and take a snapshot of the new state of the library
+#' install.packages("TTR")
+#' snapshot()
+#' 
+#' # Accidentally remove a package and restore to add it back
+#' remove.packages("TTR")
+#' restore()
+#' }
+#' 
+#' @docType package
+#' @name packrat
+#' @author RStudio, Inc.
 #' @importFrom RJSONIO fromJSON toJSON
 NULL
 
 #' Initialize Packrat on a new or existing R project
 #' 
-#' Given an application directory, computes the application's 
-#' dependencies, and places the application's dependencies under packrat
-#' control.
+#' Given an application directory, makes a new packrat project in the directory.
 #' 
-#' You must restart your R session after running bootstrap in order for
-#' the changes to take effect.
+#' \code{bootstrap} works as follows: \enumerate{ \item Application dependencies
+#' are computed by examining the R code as described in
+#' \code{\link{appDependencies}}.
+#' 
+#' \item A snapshot is taken of the version of each package currently used by 
+#' the application as described in \code{\link{snapshot}}, and each package's 
+#' sources are downloaded.
+#' 
+#' \item A private library is created in the directory.
+#' 
+#' \item The snapshot is applied to the directory as described in 
+#' \code{\link{restore}}. } When \code{bootstrap} is finished, all the packages
+#' on which the project depends are installed in a new, private library located
+#' inside the project directory.
+#' 
+#' You must restart your R session in the given application directory after 
+#' running \code{bootstrap} in order for the changes to take effect.
+#' 
+#' When R is started in the directory, it will use the new, private library by
+#' default whenever you make library changes (using 
+#' \code{\link{install.packages}}, etc.). You can sync this private library with
+#' packrat using \code{\link{snapshot}} and \code{\link{restore}}.
 #' 
 #' @param appDir The directory that contains the R project.
-#' @param sourcePackagePaths Optional list of paths to unpacked R
-#'   package source directories.
+#' @param sourcePackagePaths List of paths to unpacked R package source 
+#'   directories.  Use this argument only if your project depends on packages
+#'   that are not available on CRAN or Github.
+#'   
+#' @seealso \link{packrat} for a description of the files created by
+#'   \code{bootstrap}.
 #'   
 #' @export
 bootstrap <- function(appDir = '.', sourcePackagePaths = character()) {
@@ -48,6 +143,60 @@ bootstrap <- function(appDir = '.', sourcePackagePaths = character()) {
   invisible()
 }
 
+#' Apply the most recent snapshot to the library
+#'
+#' Applies the most recent snapshot to the project's private library. 
+#'
+#' \code{restore} works by adding, removing, and changing packages so that the
+#' set of installed packages and their versions matches the snapshot exactly.
+#' 
+#' There are three common use cases for \code{restore}: 
+#' \itemize{
+#'   \item \strong{Hydrate}: Use \code{restore} after copying a project to a new
+#' machine to populate the library on that machine. 
+#' 
+#'   \item \strong{Sync}: Use \code{restore} to apply library changes made by a
+#' collaborator to your own library. (In general, you want to run \code{restore}
+#' whenever you pick up a change to \code{packrat.lock}) 
+#' 
+#'   \item \strong{Rollback}: Use \code{restore} to undo accidental changes made
+#' to the library since the last snapshot.
+#' }
+#' 
+#' \code{restore} cannot make changes to packages that are currently loaded. If 
+#' changes are necessary to currently loaded packages, you will need to restart
+#' R to apply the changes (\code{restore} will let you know when this is
+#' necessary). It is recommended that you do this as soon as possible, because
+#' any library changes made between running \code{restore} and restarting R will
+#' be lost.
+#' 
+#' @note
+#' \code{restore} can be destructive; it will remove packages that were not in
+#' the snapshot, and it will replace newer packages with older versions if
+#' that's what the snapshot indicates. \code{restore} will warn you before
+#' attempting to remove or downgrade a package (if \code{prompt} is
+#' \code{TRUE}), but will always perform upgrades and new installations without
+#' prompting.
+#' 
+#' \code{restore} works only on the private package library created by packrat;
+#' if you have other libraries on your path, they will be unaffected.
+#' 
+#' @param appDir The directory that contains the packrat project.
+#' @param overwriteDirty A dirty package is one that has been changed since the
+#' last snapshot or restore. Packrat will leave these alone by default. If you
+#' want to guarantee that \code{restore} will put you in the exact state
+#' represented by the snapshot being applied, use \code{overwriteDirty = TRUE}.
+#' @param prompt \code{TRUE} to prompt before performing potentially destructive
+#' changes (package removals or downgrades); \code{FALSE} to perform these
+#' operations without confirmation.
+#'
+#' @seealso
+#' \code{\link{snapshot}}, the command that creates the snapshots applied with
+#' \code{restore}.
+#' 
+#' \code{\link{status}} to view the differences between the most recent snapshot
+#' and the library.
+#' 
 #' @export
 restore <- function(appDir = '.', overwriteDirty = FALSE, 
                     prompt = interactive()) {
@@ -103,6 +252,26 @@ restore <- function(appDir = '.', overwriteDirty = FALSE,
               pkgsToKeep = dirtyPackageNames, prompt = prompt)
 }
 
+#' Show differences between the last snapshot and the library
+#'
+#' Shows the differences between the project's packrat dependencies, its private
+#' package library, and its R scripts.
+#'
+#' These differences are created when you use the normal R package management
+#' commands like \code{\link{install.packages}}, \code{\link{update.packages}},
+#' and \code{\link{remove.packages}}. To bring these differences into packrat, you
+#' can use \code{\link{snapshot}}.
+#' 
+#' Differences can also arise if one of your collaborators adds or removes
+#' packages from the packrat dependencies. In this case, you simply need to tell
+#' packrat to update your private package library using \code{\link{restore}}.
+#'
+#' @param appDir The directory that contains the R project.
+#' @param lib.loc The library to examine. Defaults to the private library
+#' associated with the application directory.
+#' @param quiet \code{TRUE} to suppress output, \code{FALSE} (the default) to
+#' show output.
+#' 
 #' @export
 status <- function(appDir = '.', lib.loc = libdir(appDir), quiet = FALSE) {
   appDirDefault <- identical(appDir, '.')
@@ -298,6 +467,29 @@ prettyPrintPair <- function(packagesFrom, packagesTo, header, footer = NULL,
 }
 
 #' Remove unused packages
+#' 
+#' Remove unused packages from the given library.
+#' 
+#' Detects and removes orphaned packages. Orphaned packages are those that meet
+#' the following criteria: 
+#' \itemize{
+#'   \item Installed in the library
+#'   \item Not directly used by any R code in the application
+#'   \item Not a dependency of any non-orphaned package
+#' }
+#' If \code{clean} wants to remove a package but your project actually needs the
+#' package, add a statement such as \code{\link{require}(package-name)} to any .R
+#' file in your project's directory. 
+#'
+#' @param appDir Directory containing application. Defaults to current working
+#' directory.
+#' @param lib.loc The library to clean. Defaults to the private package library
+#' associated with the application directory.
+#' @param prompt \code{TRUE} to prompt before removing packages, \code{FALSE} to
+#' remove packages immediately.
+#' 
+#' @seealso \code{\link{appDependencies}} for an explanation of how dependencies are detected.
+#'
 #' @export
 clean <- function(appDir = ".", lib.loc = libdir(appDir),
                   prompt = interactive()) {
@@ -354,8 +546,24 @@ wipe <- function(appDir = getwd()) {
   return(invisible())
 }
 
+#' Install packrat startup directives
+#' 
 #' Install .Rprofile and .Renviron files in the given directory to make it
 #' use a private package library.
+#' 
+#' Packrat uses entries in the \code{.Rprofile} and \code{.Renviron} files to keep
+#' package library operations (such as \code{\link{install.packages}}) local to
+#' the project's local library. 
+#' 
+#' It is not normally necessary to call \code{packify} directly; these files are
+#' normally installed by \code{\link{bootstrap}}. \code{packify} can be used to
+#' restore the files if they are missing (for instance, if they were not added to
+#' source control, or were accidentally removed). 
+#' 
+#' You'll need to restart R in the specified directory after running
+#' \code{packify} in order to start using the private package library.
+#' 
+#' @param dir The directory in which to install .Rprofile and .Renviron files.
 #' 
 #' @export
 packify <- function(dir = '.') {
@@ -428,6 +636,21 @@ deaugmentFile <- function(file, delete.if.empty=TRUE) {
   return(invisible())
 }
 
+#' Show the path to the current private library
+#'
+#' Returns the path to the private package library used by packrat.
+#'
+#' @param appDir Directory containing application. Defaults to current working
+#' directory.
+#' @return A character vector containing the path to the private package library. The path
+#' is not guaranteed to exist on disk.
+#'
+#' @note
+#' The private package library is normally created by \code{\link{bootstrap}}.
+#' @examples
+#' # Show the library directory for the current working directory
+#' libdir()
+#' 
 #' @export
 libdir <- function(appDir = ".") {
   file.path(normalizePath(appDir, winslash='/', mustWork=TRUE), 'library', 
