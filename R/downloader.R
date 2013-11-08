885a9114965b775ee0eb20086924f6c0747045fa
Joe Cheng <joe@rstudio.org>
2013-11-08 11:55:28 -0800

    Remove httr dependency

---
+++ R/downloader.R
@@ -0,0 +1,83 @@
+# Download a file, using http, https, or ftp
+#
+# This is a wrapper for \code{\link{download.file}} and takes all the same
+# arguments. The only difference is that, if the protocol is https, it changes
+# some settings to make it work. How exactly the settings are changed
+# differs among platforms.
+#
+# This function also should follow http redirects on all platforms, which is
+# something that does not happen by default when \code{curl} is used, as on
+# Mac OS X.
+#
+# With Windows, it calls \code{setInternet2}, which tells R to use the
+# \code{internet2.dll}. Then it downloads the file by calling
+# \code{\link{download.file}} using the \code{"internal"} method.
+#
+# On other platforms, it will try to use \code{wget}, then \code{curl}, and
+# then \code{lynx} to download the file. Typically, Linux platforms will have
+# \code{wget} installed, and Mac OS X will have \code{curl}.
+#
+# Note that for many (perhaps most) types of files, you will want to use
+# \code{mode="wb"} so that the file is downloaded in binary mode.
+#
+# @param url The URL to download.
+# @param ... Other arguments that are passed to \code{\link{download.file}}.
+#
+# @seealso \code{\link{download.file}} for more information on the arguments
+#   that can be used with this function.
+#
+# @examples
+# \dontrun{
+# # Download the downloader source, in binary mode
+# download("https://github.com/wch/downloader/zipball/master",
+#          "downloader.zip", mode = "wb")
+# }
+#
+download <- function(url, ...) {
+  # First, check protocol. If http or https, check platform:
+  if (grepl('^https?://', url)) {
+    
+    # If Windows, call setInternet2, then use download.file with defaults.
+    if (.Platform$OS.type == "windows") {
+      
+      # If we directly use setInternet2, R CMD CHECK gives a Note on Mac/Linux
+      seti2 <- `::`(utils, 'setInternet2')
+      
+      # Store initial settings, and restore on exit
+      internet2_start <- seti2(NA)
+      on.exit(suppressWarnings(seti2(internet2_start)))
+      
+      # Needed for https. Will get warning if setInternet2(FALSE) already run
+      # and internet routines are used. But the warnings don't seem to matter.
+      suppressWarnings(seti2(TRUE))
+      download.file(url, ...)
+      
+    } else {
+      # If non-Windows, check for curl/wget/lynx, then call download.file with
+      # appropriate method.
+      
+      if (nzchar(Sys.which("wget")[1])) {
+        method <- "wget"
+      } else if (nzchar(Sys.which("curl")[1])) {
+        method <- "curl"
+        
+        # curl needs to add a -L option to follow redirects.
+        # Save the original options and restore when we exit.
+        orig_extra_options <- getOption("download.file.extra")
+        on.exit(options(download.file.extra = orig_extra_options))
+        
+        options(download.file.extra = paste("-L", orig_extra_options))
+        
+      } else if (nzchar(Sys.which("lynx")[1])) {
+        method <- "lynx"
+      } else {
+        stop("no download method found")
+      }
+      
+      download.file(url, method = method, ...)
+    }
+    
+  } else {
+    download.file(url, ...)
+  }
+}
