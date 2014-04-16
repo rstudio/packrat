rstudio/packrat

commit 6e1790fd9d934f7a330d54198233ab0e40a769ac

don't write lock file until snapshot is complete to avoid inconsistency between sources and lock file

R/snapshot.R
@@ -168,9 +168,9 @@ snapshotImpl <- function(projDir = '.', available = NULL, lib.loc = libdir(projD
   }
   
   if (!dry.run) {
+    snapshotSources(projDir, activeRepos(), appPackages)
     writeLockFile(file.path(projDir, "packrat.lock"),
                   appPackages)
-    snapshotSources(projDir, activeRepos(), lockInfo(projDir))
     cat('Snapshot written to', 
         normalizePath(file.path(projDir, "packrat.lock"), winslash = '/'), '\n')
   }

commit 49c0e8f1b5f08306765f634bb6122b2302443a50

also fetch sources for dependencies
regression introduced in 6e1790fd9d934f

R/snapshot.R
@@ -168,7 +168,7 @@ snapshotImpl <- function(projDir = '.', available = NULL, lib.loc = libdir(projD
   }
   
   if (!dry.run) {
-    snapshotSources(projDir, activeRepos(projDir), appPackages)
+    snapshotSources(projDir, activeRepos(projDir), flattenPackageRecords(appPackages, depInfo=TRUE, sourcePath=TRUE))
     writeLockFile(file.path(projDir, "packrat.lock"),
                   appPackages)
     cat('Snapshot written to', 