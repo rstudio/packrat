6c53e7611f34589a3b0230d3f337f6859f0f8266
Kevin Ushey <kevinushey@gmail.com>
2014-05-13 16:56:26 -0700

    Initial bootstrap code

--- R/aaa-global.R
+++ R/aaa-global.R
@@ -1,8 +1,8 @@
 .packrat <- new.env(parent = emptyenv())
 
-# 'Global' options that we might want to allow users to set
-.packrat$packratFolderName <- ".packrat"
-
 # Settings seen when packrat is loaded (used when turning packrat mode on and off)
-.packrat$promptOnLoad <- getOption("prompt")
-.packrat$origLibPaths <- .libPaths()
+.onLoad <- function(libname, pkgname) {
+  .packrat$packratFolderName <- ".packrat"
+  .packrat$promptOnLoad <- getOption("prompt")
+  .packrat$origLibPaths <- .libPaths()
+}
