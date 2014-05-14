efa6b40dadd0deeef7a94c58aa100e47af52c4ed
Kevin Ushey <kevinushey@gmail.com>
2014-05-14 11:57:47 -0700

    Quietly check for packrat

--- inst/init.R
+++ inst/init.R
@@ -1,6 +1,5 @@
-if (require("packrat")) {
+if (suppressWarnings(require("packrat", quietly = TRUE))) {
   packrat::packrat_mode()
 } else {
   message("error: packrat not installed; cannot enter packrat mode")
 }
-
