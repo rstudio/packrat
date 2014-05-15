706300b5b82f07e29cd6da9a5eae13fc38fafa26
Kevin Ushey <kevinushey@gmail.com>
2014-05-15 13:26:13 -0700

    Cleaner test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,20 @@
 context("utils")
 
 test_that("dir_copy copies directories", {
+
+  # Work in temporary directory
+  owd <- getwd()
+  on.exit(setwd(owd))
+  setwd(tempdir())
+
+  # Create a directory and try to copy it
   dir.create("foo")
+
   file.create("foo/foo1.R")
   file.create("foo/foo2.R")
   file.create("foo/foo3.R")
   file.create("foo/.dotFile")
+
   dir_copy("foo", "bar", overwrite = TRUE)
   expect_identical(
     list.files("foo"),
