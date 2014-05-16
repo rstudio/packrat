8ae9ec12b538e0bcdd72be228726f9c7aa5b6a58
Kevin Ushey <kevinushey@gmail.com>
2014-05-16 13:12:07 -0700

    Add @jcheng5's library migration script

---
+++ R/mac-migrate.R
@@ -0,0 +1,54 @@
+userPkgsInSystemLibrary <- function() {
+  systemPkgs <- as.data.frame(installed.packages(tail(.libPaths(), 1)), stringsAsFactors = FALSE)
+  userLibs <- with(systemPkgs, {
+    Package[is.na(Priority)]
+  })
+  userLibs
+}
+
+checkDirtySystemLibrary <- function() {
+  userPkgs <- userPkgsInSystemLibrary()
+  length(userPkgs) > 0
+}
+
+checkNeedsLibraryMigration <- function() {
+
+  shouldRun <- checkDirtySystemLibrary()
+  if (!shouldRun) {
+    return(invisible(FALSE))
+  } else {
+    scriptCall <- paste("source", system.file(package = "packrat", "mac_r_userlib.sh"))
+    message(paste( sep = "",
+                   "WARNING: It appears you have user libraries installed in your system library.\n",
+                   "Packrat relies on only 'base' and 'recommended' R packages being installed ",
+                   "in the system library."))
+    switch (Sys.info()["sysname"],
+            "Darwin"=macLibraryMigration(),
+            "Windows"=windowsLibraryMigration(), ## TODO: windows-specific migration?
+            defaultLibraryMigration()
+    )
+  }
+
+}
+
+## TODO -- how should we instruct the user to migrate in the default case?
+defaultLibraryMigration <- function() {
+  message(paste(sep = "",
+                "Please migrate the following packages to a user library:\n- ",
+                paste(userPkgsInSystemLibrary(), collapse = ", ")
+  ))
+}
+
+## TODO -- migrate on Windows?
+windowsLibraryMigration <- function() {
+  defaultLibraryMigration()
+}
+
+macLibraryMigration <- function() {
+  scriptCall <- paste("bash", system.file(package = "packrat", "mac_r_userlib.sh"))
+  message(paste(sep = "",
+                "Please run the following command at a terminal to migrate your user libraries to a user library folder:\n",
+                "    ", scriptCall))
+}
+
+
