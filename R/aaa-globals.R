3414b5e8a944d6123ea0a48946ed682a3c50cfb0
Kevin Ushey <kevinushey@gmail.com>
2014-05-16 08:52:21 -0700

    Don't hide the packrat folder

--- R/aaa-globals.R
+++ R/aaa-globals.R
@@ -3,7 +3,7 @@
 # Settings seen when packrat is loaded (used when turning packrat mode on and off)
 .onLoad <- function(libname, pkgname) {
   .packrat$projectDir <- "." ## should be set when a user enters packrat mode; but defaults to current dir
-  .packrat$packratFolderName <- ".packrat"
+  .packrat$packratFolderName <- "packrat"
   .packrat$promptOnLoad <- getOption("prompt")
   .packrat$origLibPaths <- .libPaths()
 }
