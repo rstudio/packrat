63db2e97421f60cabddfd23fc2ad1b7d394bcaec
Kevin Ushey <kevinushey@gmail.com>
2014-05-19 23:07:51 -0700

    Some tweaks; build packages from source instead of just copying

--- R/updateInit.R
+++ R/updateInit.R
@@ -1,8 +1,8 @@
 updateInit <- function() {
-  init.R <- readLines("inst/init.R")
-  packrat.version <- readDcf("DESCRIPTION")[1, "Version"]
+  init.R <- readLines(file.path("inst", "init.R"))
+  packrat.version <- read.dcf("DESCRIPTION")[1, "Version"]
   init.R[1] <- paste(sep = "",
                      "#### -- Packrat Autoloader (version ", packrat.version, ") -- ####")
   init.R[length(init.R)] <- "#### -- End Packrat Autoloader -- ####"
-  cat(init.R, file="inst/init.R", sep = "\n")
+  cat(init.R, file=file.path("inst", "init.R"), sep = "\n")
 }
--- inst/init.R
+++ inst/init.R
@@ -4,8 +4,8 @@ if (suppressWarnings(require("packrat", quietly = TRUE))) {
   packrat::packrat_mode()
 } else {
   message("error: packrat not installed; cannot enter packrat mode")
-  if (file.exists(".packrat/bootstrap.R")) {
-    message("Run 'source(\".packrat/bootstrap.R\")' to bootstrap a packrat installation.")
+  if (file.exists('packrat/bootstrap.R')) {
+    message("Run 'source(\"packrat/bootstrap.R\")' to bootstrap a packrat installation.")
   }
 }
 #### -- End Packrat Autoloader -- ####
