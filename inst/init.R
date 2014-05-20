1721fa24db05dcba13d9d3d1a582f982c9548373
Kevin Ushey <kevinushey@gmail.com>
2014-05-20 10:46:52 -0700

    Async automatic snapshots; when bootstrap use projDir as potential source path; more automatic update of init.R

--- inst/init.R
+++ inst/init.R
@@ -1,11 +1,12 @@
 #### -- Packrat Autoloader (version 0.1.0.99) -- ####
 if (suppressWarnings(require("packrat", quietly = TRUE))) {
   packrat:::checkPackified()
-  packrat::packrat_mode()
+  if (!packrat:::packratModeOn()) {
+    packrat::packrat_mode()
+  }
 } else {
   message("error: packrat not installed; cannot enter packrat mode")
-  if (file.exists('packrat/bootstrap.R')) {
-    message("Run 'source(\"packrat/bootstrap.R\")' to bootstrap a packrat installation.")
-  }
+  if (file.exists('packrat/bootstrap.R')) ## -- packrat::bootstrap_path -- ##
+    message('Run \'source("packrat/bootstrap.R")\' to bootstrap a packrat installation.') ## -- packrat::bootstrap_message -- ##
 }
 #### -- End Packrat Autoloader -- ####
