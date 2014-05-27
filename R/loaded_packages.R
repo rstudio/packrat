e8ed14dd9f4598933348c03657fac929bd132dfe
kevinushey <kevinushey@gmail.com>
2014-05-27 10:03:57 -0700

    loaded_packages: Get packages + associated library (#56)

---
+++ R/loaded_packages.R
@@ -0,0 +1,33 @@
+##' Get Packages on the Search Path
+##'
+##' This function retrieves the packages on the search path, as well as their
+##' associated library location.
+##'
+##' @export
+loaded_packages <- function() {
+
+  ## Start by getting everything on the search path + the library location
+  pkgs <- data.frame(
+    package = search(),
+    lib.loc = searchpaths(),
+    stringsAsFactors = FALSE
+  )
+
+  ## Filter to only actual packages
+  pkgs <- pkgs[ grep("^package:", pkgs$package), ]
+
+  ## Clean up the package name by removing the initial 'package:'
+  pkgs$package <- sub("^package:", "", pkgs$package)
+
+  ## Unset the rownames
+  rownames(pkgs) <- NULL
+
+  ## Get just the directory containing the library, not the library path itself
+  pkgs$lib.dir <- dirname(pkgs$lib.loc)
+
+  ## Arrange by lib.dir
+  pkgs <- pkgs[ order(pkgs$lib.dir), ]
+
+  pkgs
+
+}
