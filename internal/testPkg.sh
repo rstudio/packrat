5e55e6219f32f7d28c8a6695ad4e7b5f540f7e84
kevinushey <kevinushey@gmail.com>
2014-06-02 10:29:11 -0700

    Use digest as our 'sample R package'

--- internal/testPkg.sh
+++ internal/testPkg.sh
@@ -6,6 +6,6 @@ mkdir testPkg
 cd testPkg
 touch foo.R bar.R baz.R
 echo "library(digest)" >> foo.R
-cp ~/git/knitr/DESCRIPTION ~/git/testPkg/DESCRIPTION
+cp ~/git/digest/DESCRIPTION ~/git/testPkg/DESCRIPTION
 R --vanilla -e "options(repos = c(CRAN = 'http://cran.rstudio.org')); library(packrat); dir.create('.git'); bootstrap(source='~/git/packrat')"
 R
