c1019a146f7565f2ac3e929a27628c5747d0f444
kevinushey <kevinushey@gmail.com>
2014-06-04 15:02:27 -0700

    Tweak test package script a tiny bit

--- internal/digestPkg.sh
+++ internal/digestPkg.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
 ## Testing a package using a source package tarball
+PACKAGE_NAME="digestPkg"
 cd ~/git
 R CMD build packrat && R CMD INSTALL packrat_0.2.0.99.tar.gz
-rm -rf digestPkg
-mkdir digestPkg
-cd digestPkg
+rm -rf ${PACKAGE_NAME}
+mkdir ${PACKAGE_NAME}
+cd ${PACKAGE_NAME}
 touch foo.R bar.R baz.R
 echo "library(digest)" >> foo.R
-cp ~/git/digest/DESCRIPTION ~/git/testPkg/DESCRIPTION
+cp ~/git/packrat/internal/digestPkg.Rproj ~/git/${PACKAGE_NAME}/
 R --vanilla -e "options(repos = c(CRAN = 'http://cran.rstudio.org')); library(packrat); dir.create('.git'); bootstrap(source=c('~/git/packrat', '~/git/digest_0.6.4.1.tar.gz'))"
 R
