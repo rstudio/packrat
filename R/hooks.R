2c81b21405eeb4b38e6ac2b198ac19e07e0e42e8
kevinushey <kevinushey@gmail.com>
2014-06-05 15:03:13 -0700

    Refactor snapshot hook; make sure repositories are passed

--- R/hooks.R
+++ R/hooks.R
@@ -3,46 +3,9 @@
 snapshotHook <- function(expr, value, ok, visible) {
 
   tryCatch(
+
     expr = {
-
-      project <- getProjectDir()
-      packratDir <- getPackratDir(project)
-
-      ## A snapshot lock file that we should check to ensure we don't try to
-      ## snapshot multiple times
-      snapshotLockPath <- file.path(packratDir, "snapshot.lock")
-
-      ## This file needs to be checked, and deleted, by the async process
-      if (file.exists(snapshotLockPath)) {
-        return(FALSE)
-      }
-      file.create(snapshotLockPath)
-
-      peq <- function(x, y) paste(x, y, sep = " = ")
-      snapshotArgs <- paste(sep = ", ",
-                            peq("project", shQuote(project)),
-                            peq("orphan.check", "FALSE"),
-                            peq("auto.snapshot", "TRUE"),
-                            peq("verbose", "FALSE")
-      )
-      setwdCmd <- paste("setwd(", shQuote(project), ")")
-      snapshotCmd <- paste("try(suppressMessages(packrat:::snapshotImpl(", snapshotArgs, ")))")
-      cleanupCmd <- paste("file.remove(", shQuote(snapshotLockPath), ")")
-      setLibsCmd <- paste(".libPaths( c(", paste(shQuote(getLibPaths()), collapse = ", "), ") )")
-      fullCmd <- paste(sep = "; ",
-                       setwdCmd,
-                       setLibsCmd,
-                       snapshotCmd,
-                       cleanupCmd,
-                       "invisible()"
-      )
-
-      r_path <- file.path(R.home("bin"), "R")
-
-      cmd <- paste(shQuote(r_path), "--vanilla", "--slave", "-e", shQuote(fullCmd))
-      res <- system(cmd, wait = FALSE, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
-      invisible(TRUE)
-
+      snapshotHookImpl()
     },
 
     # Cases where an automatic snapshot can fail:
@@ -73,4 +36,47 @@ snapshotHook <- function(expr, value, ok, visible) {
 
   )
 
+}
+
+snapshotHookImpl <- function() {
+  project <- getProjectDir()
+  packratDir <- getPackratDir(project)
+
+  ## A snapshot lock file that we should check to ensure we don't try to
+  ## snapshot multiple times
+  snapshotLockPath <- file.path(packratDir, "snapshot.lock")
+
+  ## This file needs to be checked, and deleted, by the async process
+  if (file.exists(snapshotLockPath)) {
+    return(FALSE) ## returning FALSE will stop the async snapshotting and can leave the snapshot.lock file stale
+  }
+  file.create(snapshotLockPath)
+
+  peq <- function(x, y) paste(x, y, sep = " = ")
+  snapshotArgs <- paste(sep = ", ",
+                        peq("project", shQuote(project)),
+                        peq("orphan.check", "FALSE"),
+                        peq("auto.snapshot", "TRUE"),
+                        peq("verbose", "FALSE")
+  )
+  setwdCmd <- paste("setwd(", shQuote(project), ")")
+  snapshotCmd <- paste("try(suppressMessages(packrat:::snapshotImpl(", snapshotArgs, ")))")
+  cleanupCmd <- paste("file.remove(", shQuote(snapshotLockPath), ")")
+  setLibsCmd <- paste(".libPaths( c(", paste(shQuote(getLibPaths()), collapse = ", "), ") )")
+  repos <- paste(capture.output(dput(getOption('repos'))), collapse = " ")
+  reposCmd <- paste("options('repos' = ", repos, ")", sep = "")
+  fullCmd <- paste(sep = "; ",
+                   setwdCmd,
+                   reposCmd,
+                   setLibsCmd,
+                   snapshotCmd,
+                   cleanupCmd,
+                   "invisible()"
+  )
+
+  r_path <- file.path(R.home("bin"), "R")
+
+  cmd <- paste(shQuote(r_path), "--vanilla", "--slave", "-e", shQuote(fullCmd))
+  res <- system(cmd, wait = FALSE, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
+  invisible(TRUE)
 }
