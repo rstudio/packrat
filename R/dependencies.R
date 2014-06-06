bbe087f1a137d64c0ac98ed4dd5de7738281ec46
kevinushey <kevinushey@gmail.com>
2014-06-06 16:35:45 -0700

    Make sure we grab sub-dependencies of packages we find

--- R/dependencies.R
+++ R/dependencies.R
@@ -47,7 +47,9 @@ appDependencies <- function(project = NULL, available.packages = NULL) {
                                               available.packages)
     sort(unique(c(descriptionDeps, childDeps)))
   } else {
-    unique(c(dirDependencies(project), 'packrat'))
+    parentDeps <- unique(c(dirDependencies(project), 'packrat'))
+    childDeps <- recursivePackageDependencies(parentDeps, available.packages)
+    sort(unique(c(parentDeps, childDeps)))
   }
 
 }
