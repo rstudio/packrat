90ea602f2639c2d166827e03d52055bdbfe5f06a
Kevin Ushey <kevinushey@gmail.com>
2014-06-08 17:01:26 -0700

    Consolidate init.R and bootstrap.R into a single file

--- inst/resources/init.R
+++ inst/resources/init.R
@@ -1,24 +1,3 @@
 #### -- Packrat Autoloader (version 0.2.0.99) -- ####
-local({
-
-  ## -- packrat::libDir -- ##
-  libDir <- file.path('packrat', 'lib', R.version$platform, getRversion())
-
-  if (suppressWarnings(require("packrat", quietly = TRUE, lib.loc = libDir))) {
-    packrat:::checkPackified()
-    packrat:::setPackratModeOn()
-    packrat:::updateSettings()
-  } else {
-    message("Error: packrat not installed; cannot enter packrat mode")
-
-    ## -- packrat::bootstrapPath -- ##
-    bootstrapPath <- 'packrat/bootstrap.R'
-
-    if (file.exists(bootstrapPath)) {
-
-      ## -- packrat::bootstrapMessage -- ##
-      message('Run \'source("packrat/bootstrap.R")\' to bootstrap this project.')
-    }
-  }
-})
+source(file.path("packrat", "bootstrap.R"))
 #### -- End Packrat Autoloader -- ####
