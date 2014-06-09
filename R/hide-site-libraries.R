ec09b337e3f34ba78ec0d40bc7e27ebfd604361d
kevinushey <kevinushey@gmail.com>
2014-06-09 14:52:28 -0700

    Package isolation on Mac
    We do a few things in this set of changes:
    1. We symlink all 'base' and 'recommended' packages into a
    'packrat/lib-R' library directory, and
    2. Overwrite .Library to point at that directory.
    
    By doing so, on Mac, we now have the packrat private library, and the
    symlinked system library, which will now no longer pick up user
    libraries that are installed there.
    
    This would then obviate the need for library migration.

--- R/hide-site-libraries.R
+++ R/hide-site-libraries.R
@@ -1,25 +1,30 @@
+hideLibrary <- function(lib) {
+  do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
+  assign(lib, character(), envir = .BaseNamespaceEnv)
+  lockBinding(lib, .BaseNamespaceEnv)
+}
+
+restoreLibrary <- function(lib) {
+  oldLibs <- .packrat_mutables$get(lib)
+  if (!is.null(oldLibs)) {
+    do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
+    assign(lib, oldLibs, envir = .BaseNamespaceEnv)
+    lockBinding(lib, .BaseNamespaceEnv)
+  }
+}
+
+replaceLibrary <- function(lib, value) {
+  do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
+  assign(lib, value, envir = .BaseNamespaceEnv)
+  lockBinding(lib, .BaseNamespaceEnv)
+}
+
 ## Remove the site-library libraries from unix-alikes
 hideSiteLibraries <- function() {
-
-  .packrat_mutables$set(.Library.site = .Library.site)
-
-  ## This is necessary for functionality in packrat, and we want to hide the NOTE
-  do.call("unlockBinding", list(".Library.site", .BaseNamespaceEnv))
-  assign(".Library.site", character(), envir = .BaseNamespaceEnv)
-  lockBinding(".Library.site", .BaseNamespaceEnv)
-
-  ## Reset the getLibPaths()
-  setLibPaths(character())
+  hideLibrary(".Library.site")
 }
 
 ## Restore the site-library libraries
 restoreSiteLibraries <- function() {
-
-  oldSiteLibs <- .packrat_mutables$get(".Library.site")
-  if (!is.null(oldSiteLibs)) {
-    do.call("unlockBinding", list(".Library.site", .BaseNamespaceEnv))
-    assign(".Library.site", oldSiteLibs, envir = .BaseNamespaceEnv)
-    lockBinding(".Library.site", .BaseNamespaceEnv)
-  }
-
+  restoreLibrary(".Library.site")
 }
