e24df4bc24ba76324c63aaf74d429551da73fc9b
kevinushey <kevinushey@gmail.com>
2014-06-10 11:42:47 -0700

    Some refactoring

--- R/hide-site-libraries.R
+++ R/hide-site-libraries.R
@@ -1,22 +1,34 @@
+replaceLibrary <- function(lib, value) {
+
+  ## Need to clobber in package:base, namespace:base
+  envs <- c(
+    as.environment("package:base"),
+    .BaseNamespaceEnv
+  )
+
+  for (env in envs) {
+    do.call("unlockBinding", list(lib, env))
+    assign(lib, value, envir = env)
+    lockBinding(lib, env)
+  }
+
+}
+
+
 hideLibrary <- function(lib) {
-  do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
-  assign(lib, character(), envir = .BaseNamespaceEnv)
-  lockBinding(lib, .BaseNamespaceEnv)
+  replaceLibrary(lib, character())
 }
 
 restoreLibrary <- function(lib) {
-  oldLibs <- .packrat_mutables$get(lib)
-  if (!is.null(oldLibs)) {
-    do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
-    assign(lib, oldLibs, envir = .BaseNamespaceEnv)
-    lockBinding(lib, .BaseNamespaceEnv)
-  }
-}
 
-replaceLibrary <- function(lib, value) {
-  do.call("unlockBinding", list(lib, .BaseNamespaceEnv))
-  assign(lib, value, envir = .BaseNamespaceEnv)
-  lockBinding(lib, .BaseNamespaceEnv)
+  cachedLib <- .packrat_mutables$get(lib)
+  if (is.null(cachedLib)) {
+    warning("packrat did not properly save the library state; cannot restore")
+    return(invisible(NULL))
+  }
+
+  replaceLibrary(lib, cachedLib)
+
 }
 
 ## Remove the site-library libraries from unix-alikes
