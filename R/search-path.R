6a2b94e5b52016c93d7b14e7a6fffe76619ce534
Kevin Ushey <kevinushey@gmail.com>
2014-06-09 17:44:04 -0700

    Use find.package for better resolution of base, recommended packages

--- R/search-path.R
+++ R/search-path.R
@@ -6,18 +6,25 @@
 ##' @export
 search_path <- function() {
 
+  ## NOTE: We cannot use searchpaths() here because it follows symlinks --
+  ## for consistency, we want to use the symlink, not the actual path
+
   ## Start by getting everything on the search path + the library location
   pkgs <- data.frame(
     package = search(),
-    lib.loc = searchpaths(),
     stringsAsFactors = FALSE
   )
 
   ## Filter to only actual packages
-  pkgs <- pkgs[ grep("^package:", pkgs$package), ]
+  pkgs <- pkgs[ grep("^package:", pkgs$package), , drop = FALSE]
 
   ## Clean up the package name by removing the initial 'package:'
   pkgs$package <- sub("^package:", "", pkgs$package)
+
+  ## Get the library locations
+  ## NOTE: find.package is special-cased when the first argument is
+  ## of length 1 -- we rely on that behaviour here
+  pkgs$lib.loc <- unlist(lapply(pkgs$package, find.package))
 
   ## Get just the directory containing the library, not the library path itself
   pkgs$lib.dir <- dirname(pkgs$lib.loc)
