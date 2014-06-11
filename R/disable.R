7ef7f9fd4f82a3de0dbe19cc9cc0808ffe55f7a6
JJ Allaire <jj@rstudio.org>
2014-06-11 08:42:04 -0400

    rename remove function to disable

---
+++ R/disable.R
@@ -0,0 +1,38 @@
+
+#' Disable the use of Packrat in a Project
+#'
+#' Disable packrat within a project, reverting to the use of standard user
+#' package libraries.
+#'
+#' @param project The directory in which packrat will be disabled (defaults to
+#'   the current working directory)
+#' @param restart If \code{TRUE}, restart the R session after disabling packrat.
+#'
+#' @note Disabling packrat for a project removes the packrat initialization code
+#' from the .Rprofile file, resulting in the use of standard user package
+#' libraries. Note that the \code{packrat} directory is not deleted, but remains
+#' unused.
+#'
+#' To re-enable the use of packrat for a project you can call the
+#' \code{\link{bootstrap}} function.
+#'
+#' The \code{restart} parameter will only result in a restart of R when the R
+#' environment packrat is running within makes available a restart function via
+#' \code{getOption("restart")}.
+#'
+#' @export
+disable <- function(project = NULL, restart = TRUE) {
+
+  # get the project
+  project <- getProjectDir(project)
+  stopIfNotPackified(project)
+
+  # remove packrat from the .Rprofile
+  editRprofileAutoloader(project, "remove")
+
+  # restart if requested
+  if (restart)
+    attemptRestart()
+
+  invisible()
+}
