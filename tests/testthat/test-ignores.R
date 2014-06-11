50cbb7cf0666b750c07f84daecc2ba761b915101
kevinushey <kevinushey@gmail.com>
2014-06-11 10:23:06 -0700

    update tests

--- tests/testthat/test-ignores.R
+++ tests/testthat/test-ignores.R
@@ -16,4 +16,48 @@ test_that("updateRBuildIgnore preserves content in ignore file", {
   updateRBuildIgnore(project = tempdir())
   content <- readLines(path)
   expect_identical(content, c("foo", "bar", "baz", "^packrat/"))
+  unlink(path)
+})
+
+test_that("updateGitIgnore works", {
+
+  options <- list(
+    vcs.ignore.lib = TRUE,
+    vcs.ignore.src = FALSE
+  )
+  dir <- file.path(tempdir(), "packrat-test")
+  .gitignore <- file.path(dir, ".gitignore")
+  dir.create(dir)
+  updateGitIgnore(project = dir, options = options)
+  content <- readLines(.gitignore)
+  expect_identical(content, "packrat/lib*/")
+
+  ## idempotency
+  updateGitIgnore(project = dir, options = options)
+  content <- readLines(.gitignore)
+  expect_identical(content, "packrat/lib*/")
+
+  ## preserve content of a .gitignore
+  unlink(.gitignore)
+  cat(c("foo", "bar", "baz"), file = .gitignore, sep = "\n")
+  updateGitIgnore(project = dir, options = options)
+  content <- readLines(.gitignore)
+  expect_true(all(c("foo", "bar", "baz", "packrat/lib*/") %in% content))
+
+  ## change options
+  options$vcs.ignore.src <- TRUE
+  updateGitIgnore(project = dir, options = options)
+  content <- readLines(.gitignore)
+  expect_true(all(c("foo", "bar", "baz", "packrat/lib*/", "packrat/src/") %in% content))
+
+  ## remove all options
+  options[] <- FALSE
+  updateGitIgnore(project = dir, options = options)
+  content <- readLines(.gitignore)
+  expect_true(all(c("foo", "bar", "baz") %in% content))
+  expect_false("packrat/lib*/" %in% content)
+  expect_false("packrat/src/" %in% content)
+
+  unlink(dir, recursive = TRUE)
+
 })
