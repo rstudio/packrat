3a8b3f1d3b5ff2abc462044ed740b6081ac7fab7
kevinushey <kevinushey@gmail.com>
2014-06-17 14:35:06 -0700

    Allow for restarts if needed on bootstrap

--- R/disable.R
+++ R/disable.R
@@ -40,7 +40,7 @@ disable <- function(project = NULL, restart = TRUE) {
 
   # restart if requested
   if (restart)
-    attemptRestart(FALSE)
+    attemptRestart(restore.packrat.mode = FALSE)
 
   invisible()
 }
