d0927eb7839de28ca6ff0ba82ac318a010dee3cd
kevinushey <kevinushey@gmail.com>
2014-06-19 11:47:01 -0700

    minor changes

--- internal/shinyPkg.sh
+++ internal/shinyPkg.sh
@@ -3,7 +3,7 @@
 ## Testing a package using a source package tarball
 PACKAGE_NAME="shinyPkg"
 cd ~/git
-R CMD build packrat && R CMD INSTALL packrat_0.2.0.100.tar.gz
+R CMD INSTALL packrat
 rm -rf ${PACKAGE_NAME}
 mkdir ${PACKAGE_NAME}
 cd ${PACKAGE_NAME}
