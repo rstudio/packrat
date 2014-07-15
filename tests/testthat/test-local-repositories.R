5bedd45e89eceff498ab0f9bdc3e9c82acb780e7
kevinushey <kevinushey@gmail.com>
2014-07-15 11:10:18 -0700

    Check that we handle awkward paths

--- tests/testthat/test-local-repositories.R
+++ tests/testthat/test-local-repositories.R
@@ -11,7 +11,10 @@ setupTestRepo()
 
 test_that("init fails when package not found in any repo", {
   projRoot <- cloneTestProject("sated")
+  repos <- getOption("repos")
+  options(repos = c(CRAN = paste0("file:///", normalizePath("repo-empty"))))
   expect_error(init(enter = FALSE, projRoot))
+  options(repos = repos)
 })
 
 test_that("init warns if a package is found in multiple local repos", {
@@ -21,4 +24,11 @@ test_that("init warns if a package is found in multiple local repos", {
 
 test_that("install_local fails if no repository has been defined", {
   expect_error(install_local("foo"))
+})
+
+test_that("packrat::get_opts can read / write atrocious paths", {
+  path <- list.files(pattern = "^Ugly")
+  opts$local.repos(path)
+  readPath <- opts$local.repos()
+  expect_identical(path, readPath)
 })
