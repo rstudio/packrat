3466f6174632210ce213a55b6c3733e7e6f5d4d3
Kevin Ushey <kevin@rstudio.com>
2014-10-17 11:04:12 -0700

    this test does not require 'git'

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -2,16 +2,13 @@ context("git")
 
 test_that("isGitProject identifies projects as sub-directories of git-managed folders as git projects", {
 
-  git <- Sys.which("git")
-  if (git != "") {
     dir <- file.path(tempdir(), "test-packrat-git")
     subdir <- file.path(dir, "subdir")
     dir.create(subdir, recursive = TRUE)
-    system2(git, c("init", dir))
+    dir.create(file.path(dir, ".git"))
     expect_true(isGitProject(dir))
     expect_true(isGitProject(subdir))
     expect_false(isGitProject(tempdir()))
     unlink(dir, recursive = TRUE)
-  }
 
 })
