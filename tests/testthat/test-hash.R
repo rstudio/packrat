ea256c053534c5c84046567f6429f0094d7737a4
Joe Cheng <joe@rstudio.com>
2015-06-11 22:48:54 -0700

    Add test
    Make sure unexported hash function is not accidentally removed
    in the future.

--- tests/testthat/test-hash.R
+++ tests/testthat/test-hash.R
@@ -6,3 +6,11 @@ test_that("we can hash packages containing multiple packages in LinkingTo", {
     hash(path)
   }
 })
+
+test_that("hash function is available and has expected arguments", {
+  # This test is a canary that you may be breaking compatibility with Connect,
+  # which expects this non-exported function to be present. That's not to say
+  # the function signature can't be changed, only that the relevant call in
+  # Connect's packrat_restore.R should be studied to avoid any breakage.
+  expect_identical(deparse(formals(hash)), "pairlist(path = , descLookup = installedDescLookup)")
+})
