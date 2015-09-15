fcaba59a72604d7f509be6db3fb5e9ed062cc4f4
Kevin Ushey <kevin@rstudio.com>
2015-09-15 12:37:07 -0700

    update version

--- inst/resources/init.R
+++ inst/resources/init.R
@@ -178,7 +178,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- 'InstallAgent: packrat 0.4.4-11'
+    installAgent <- 'InstallAgent: packrat 0.4.4-12'
 
     ## -- InstallSource -- ##
     installSource <- 'InstallSource: source'
