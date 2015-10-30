8e25c04c5f328c9310040c5e6c1bd44a1e42bf9c
Kevin Ushey <kevinushey@gmail.com>
2015-10-30 14:52:09 -0700

    ensure 'knitr' in Suggests

--- inst/resources/init.R
+++ inst/resources/init.R
@@ -178,7 +178,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- 'InstallAgent: packrat 0.4.5-5'
+    installAgent <- 'InstallAgent: packrat 0.4.5-6'
 
     ## -- InstallSource -- ##
     installSource <- 'InstallSource: source'
