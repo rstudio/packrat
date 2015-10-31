2f03fe135760b1d8e7bb939d12ccfdc14569c688
Kevin Ushey <kevin@rstudio.com>
2015-10-31 11:23:55 -0700

    bump version

--- inst/resources/init-rprofile.R
+++ inst/resources/init-rprofile.R
@@ -1,3 +1,3 @@
-#### -- Packrat Autoloader (version 0.4.5-16) -- ####
+#### -- Packrat Autoloader (version 0.4.5-17) -- ####
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
--- inst/resources/init.R
+++ inst/resources/init.R
@@ -178,7 +178,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- 'InstallAgent: packrat 0.4.5-16'
+    installAgent <- 'InstallAgent: packrat 0.4.5-17'
 
     ## -- InstallSource -- ##
     installSource <- 'InstallSource: source'
