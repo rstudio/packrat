2d9b533e7d6b81e56934e1f693dc5b50877966fd
Kevin Ushey <kevinushey@gmail.com>
2015-11-12 15:08:46 -0800

    skip Rmd document test if knitr not available

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -1,6 +1,11 @@
 context("R Markdown")
 
 test_that("Rmd documents with parameters are analyzed", {
+
+  # we need to skip this test if we don't have an up-to-date version of knitr available
+  if (packageVersion("knitr") < "1.11")
+    skip("requires knitr 1.11 or greater")
+
   parameterDocPath <- file.path("resources", "params-example.Rmd")
   deps <- packrat:::fileDependencies(parameterDocPath)
   expect_true("rmarkdown" %in% deps, "all Rmd docs have an rmarkdown dependency")
