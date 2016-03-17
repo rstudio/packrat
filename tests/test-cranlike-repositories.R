b8e1966b884a8c72f030e07c2470c1008cde9714
Kevin Ushey <kevin@rstudio.com>
2016-03-17 12:09:54 -0700

    move cran repos test outside of testthat

--- tests/test-cranlike-repositories.R
+++ tests/test-cranlike-repositories.R
@@ -1,20 +1,27 @@
-context("CRAN")
+library(packrat)
 
-test_that("We can create and 'upload' an example package", {
-  skip_on_cran() && skip_on_travis()
+(function() {
+
+  # Disable R_TESTS within this scope (we don't want the R
+  # subprocess to attempt to call startup.Rs)
+  R_TESTS <- Sys.getenv("R_TESTS")
+  Sys.unsetenv("R_TESTS")
+  on.exit(Sys.setenv(R_TESTS = R_TESTS), add = TRUE)
 
   dir <- tempdir()
-  owd <- getwd()
-  setwd(dir)
+  owd <- setwd(dir)
   on.exit(setwd(owd), add = TRUE)
+
+  # Save repos
+  repos <- getOption("repos")
+  on.exit(options(repos = repos), add = TRUE)
 
   # Create the local repo
   localCRAN <- file.path(dir, "sushi")
   packrat::repos_create(localCRAN)
 
-  repos <- getOption("repos")
-  options(repos = repos["sushi"])
-  on.exit(options(repos = repos), add = TRUE)
+  # Use only the 'sushi' repository
+  options(repos = getOption("repos")["sushi"])
 
   # Create an example package.
   env <- new.env(parent = emptyenv())
@@ -24,28 +31,24 @@ test_that("We can create and 'upload' an example package", {
   )
 
   # tidy up the broken package
-  unlink("sashimi/man", recursive = TRUE)
+  unlink(file.path(dir, "sashimi/man"), recursive = TRUE)
 
   # Try uploading the package from the directory itself (requires building)
   message("\nBuilding sashimi:\n")
-  packrat::repos_upload(
-    file.path(dir, "sashimi"),
-    "sushi"
-  )
+  packrat::repos_upload(file.path(dir, "sashimi"), "sushi")
 
   # Try building and uploading a tarball
   system(paste("R --vanilla CMD build", file.path(dir, "sashimi")))
   tarball <- list.files(dir, pattern = "\\.tar\\.gz$")[[1]]
-  packrat::repos_upload(
-    file.path(dir, tarball),
-    "sushi"
-  )
+  packrat::repos_upload(file.path(dir, tarball), "sushi")
 
   # Try installing the package as normal
   tempLib <- file.path(dir, "library")
   dir.create(tempLib)
   install.packages("sashimi", lib = tempLib)
-  library("sashimi", lib.loc = tempLib)
+
+  # avoid bogus warning from R CMD check
+  eval(call("library", "sashimi", lib.loc = tempLib))
   detach("package:sashimi", unload = TRUE)
 
-})
+})()
