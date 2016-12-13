c0d62605341ac3fd03751fb883ce3564c9ea304e
Aron Atkins <aron@gweep.net>
2016-12-13 13:00:36 -0500

    cache test now assumes packrat names are available

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -11,27 +11,22 @@ withTestContext({
     srcRoot <- file.path(projRoot,"packtar","src")
     theCache <- tempdir()
 
-    packrat::init(projRoot, options = list(local.repos = "packages"))
+    init(projRoot, options = list(local.repos = "packages"))
 
     rv <- R.Version()
-    package_dir <- file.path(projRoot,
-                             "packrat",
-                             "lib",
-                             rv$platform,
-                             getRversion(),
-                             "oatmeal")
+    package_dir <- file.path(libDir(projRoot), "oatmeal")
 
     # After the init and before we configure a cache, the package is a
     # non-symlink. dir.exists only tells us if the package is installed, as it
     # reads through symlinks.
     expect_true(dir.exists(package_dir),package_dir)
-    expect_false(packrat:::is.symlink(package_dir), package_dir)
+    expect_false(is.symlink(package_dir), package_dir)
 
     Sys.setenv(R_PACKRAT_CACHE_DIR = theCache)
     on.exit(Sys.unsetenv("R_PACKRAT_CACHE_DIR"), add = TRUE)
 
-    packrat::set_opts(use.cache = TRUE)
-    on.exit(packrat::set_opts(use.cache = FALSE), add = TRUE)
+    set_opts(use.cache = TRUE)
+    on.exit(set_opts(use.cache = FALSE), add = TRUE)
 
     options(packrat.verbose.cache = TRUE)
     on.exit(options(packrat.verbose.cache = FALSE), add = TRUE)
@@ -39,21 +34,21 @@ withTestContext({
     # Initial restore. Populates the cache and creates a symlink into it.
     unlink(libRoot, recursive = TRUE)
     unlink(srcRoot, recursive = TRUE)
-    packrat::restore(overwrite.dirty = TRUE,
+    restore(overwrite.dirty = TRUE,
                      prompt = FALSE,
                      restart = FALSE)
 
     expect_true(dir.exists(package_dir),package_dir)
-    expect_true(packrat:::is.symlink(package_dir), package_dir)
+    expect_true(is.symlink(package_dir), package_dir)
 
     # Subsequent restore. Uses the cache.
     unlink(libRoot, recursive = TRUE)
     unlink(srcRoot, recursive = TRUE)
-    packrat::restore(overwrite.dirty = TRUE,
+    restore(overwrite.dirty = TRUE,
                      prompt = FALSE,
                      restart = FALSE)
 
     expect_true(dir.exists(package_dir),package_dir)
-    expect_true(packrat:::is.symlink(package_dir), package_dir)
+    expect_true(is.symlink(package_dir), package_dir)
   })
 })
