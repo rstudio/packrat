8e6f4b3f1735f9a9df5c314308e2f62705faa49f
Kevin Ushey <kevin@rstudio.com>
2016-12-21 10:16:57 -0800

    fixups for cache tests

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,54 +1,53 @@
 context("Cache")
 
-withTestContext({
-  # https://github.com/rstudio/packrat/issues/345
-  test_that("package installation when configured with a a cache uses the cache", {
-    skip_on_cran()
-    skip_on_os("windows")
+# https://github.com/rstudio/packrat/issues/345
+test_that("package installation when configured with a a cache uses the cache", {
+  skip_on_cran()
+  skip_on_os("windows")
 
-    projRoot <- cloneTestProject("healthy")
-    libRoot <- file.path(projRoot,"packrat","lib")
-    srcRoot <- file.path(projRoot,"packtar","src")
-    theCache <- tempdir()
+  scopeTestContext()
 
-    init(projRoot, options = list(local.repos = "packages"))
+  projRoot <- cloneTestProject("healthy")
+  libRoot <- file.path(projRoot, "packrat", "lib")
+  srcRoot <- file.path(projRoot, "packrat", "src")
+  theCache <- tempdir()
 
-    rv <- R.Version()
-    package_dir <- file.path(libDir(projRoot), "oatmeal")
+  init(projRoot, options = list(local.repos = "packages"), enter = FALSE)
 
-    # After the init and before we configure a cache, the package is a
-    # non-symlink. dir.exists only tells us if the package is installed, as it
-    # reads through symlinks.
-    expect_true(dir.exists(package_dir),package_dir)
-    expect_false(is.symlink(package_dir), package_dir)
+  rv <- R.Version()
+  package_dir <- file.path(libDir(projRoot), "oatmeal")
 
-    Sys.setenv(R_PACKRAT_CACHE_DIR = theCache)
-    on.exit(Sys.unsetenv("R_PACKRAT_CACHE_DIR"), add = TRUE)
+  expect_true(file.exists(package_dir), package_dir)
+  expect_false(is.symlink(package_dir), package_dir)
 
-    set_opts(use.cache = TRUE)
-    on.exit(set_opts(use.cache = FALSE), add = TRUE)
+  Sys.setenv(R_PACKRAT_CACHE_DIR = theCache)
+  on.exit(Sys.unsetenv("R_PACKRAT_CACHE_DIR"), add = TRUE)
 
-    options(packrat.verbose.cache = TRUE)
-    on.exit(options(packrat.verbose.cache = FALSE), add = TRUE)
+  set_opts(project = projRoot, use.cache = TRUE)
+  on.exit(set_opts(use.cache = FALSE, project = projRoot), add = TRUE)
 
-    # Initial restore. Populates the cache and creates a symlink into it.
-    unlink(libRoot, recursive = TRUE)
-    unlink(srcRoot, recursive = TRUE)
-    restore(overwrite.dirty = TRUE,
-                     prompt = FALSE,
-                     restart = FALSE)
+  options(packrat.verbose.cache = TRUE)
+  on.exit(options(packrat.verbose.cache = FALSE), add = TRUE)
 
-    expect_true(dir.exists(package_dir),package_dir)
-    expect_true(is.symlink(package_dir), package_dir)
+  # Initial restore. Populates the cache and creates a symlink into it.
+  unlink(libRoot, recursive = TRUE)
+  unlink(srcRoot, recursive = TRUE)
+  restore(projRoot,
+          overwrite.dirty = TRUE,
+          prompt = FALSE,
+          restart = FALSE)
 
-    # Subsequent restore. Uses the cache.
-    unlink(libRoot, recursive = TRUE)
-    unlink(srcRoot, recursive = TRUE)
-    restore(overwrite.dirty = TRUE,
-                     prompt = FALSE,
-                     restart = FALSE)
+  expect_true(file.exists(package_dir), package_dir)
+  expect_true(is.symlink(package_dir), package_dir)
 
-    expect_true(dir.exists(package_dir),package_dir)
-    expect_true(is.symlink(package_dir), package_dir)
-  })
+  # Subsequent restore. Uses the cache.
+  unlink(libRoot, recursive = TRUE)
+  unlink(srcRoot, recursive = TRUE)
+  restore(projRoot,
+          overwrite.dirty = TRUE,
+          prompt = FALSE,
+          restart = FALSE)
+
+  expect_true(file.exists(package_dir), package_dir)
+  expect_true(is.symlink(package_dir), package_dir)
 })
