0b0192e10b3d104df906b77c1549c21ac0593936
Kevin Ushey <kevinushey@gmail.com>
2017-08-30 09:40:19 -0700

    ensure 'binary' type for install.packages()

--- inst/resources/init-rprofile.R
+++ inst/resources/init-rprofile.R
@@ -1,3 +1,3 @@
-#### -- Packrat Autoloader (version 0.4.8-33) -- ####
+#### -- Packrat Autoloader (version 0.4.8-34) -- ####
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
--- inst/resources/init.R
+++ inst/resources/init.R
@@ -181,7 +181,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- 'InstallAgent: packrat 0.4.8-33'
+    installAgent <- 'InstallAgent: packrat 0.4.8-34'
 
     ## -- InstallSource -- ##
     installSource <- 'InstallSource: source'
