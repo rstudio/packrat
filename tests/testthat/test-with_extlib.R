e5ef24f40118d9f0727a88e40f766ac98c0e517e
Bill Evans <BillEvans@ActiveDecisionSupport.com>
2018-08-13 17:45:01 -0700

    change with_extlib to work without packages
    - fixes #488, "with_extlib without the package"

---
+++ tests/testthat/test-with_extlib.R
@@ -0,0 +1,24 @@
+context("extlib without packages")
+
+test_that("with_extlib successfully works with no packages provided", {
+
+  ## Make sure packrat mode is off
+  if (packrat:::isPackratModeOn())
+    packrat::off()
+
+  orig_libs <- packrat:::getLibPaths()
+  .libPaths(c(file.path(getwd(), "packages"), orig_libs))
+  on.exit(.libPaths(orig_libs), add=TRUE)
+
+  expect_identical(packageVersion("bread"), package_version("1.0.0"))
+
+  # don't use packrat::on so we can avoid the initialization step
+  packrat:::setPackratModeOn(auto.snapshot = FALSE, clean.search.path = FALSE)
+
+  expect_error(packageVersion("bread"), "package 'bread' not found")
+
+  expect_identical(packrat::with_extlib(expr = packageVersion("bread")), package_version("1.0.0"))
+
+  packrat::off()
+
+})
