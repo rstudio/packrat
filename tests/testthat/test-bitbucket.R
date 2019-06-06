0399dc22490fa88672d8e6587b306541c86cab58
Alex Gold <alexkgold@gmail.com>
2019-06-06 15:51:45 -0400

    Fix bitbucket to be original test

--- tests/testthat/test-bitbucket.R
+++ tests/testthat/test-bitbucket.R
@@ -1,15 +1,16 @@
-context("Gitlab")
+context("Bitbucket")
 
-test_that("we can use devtools:::download to retrieve Gitlab archives", {
+test_that("we can use devtools:::download to retrieve Bitbucket archives", {
   skip("run manually for now")
 
-  if (!canUseGitlabDownloader())
+  if (!canUseBitbucketDownloader())
     skip("requires devtools")
 
-  url <- "https://gitlab.com/alexkgold/packrat-test-pkg"
-  destination <- tempfile("packrat-test-gitlab-", fileext = ".tar.gz")
-  result <- gitlabDownload(url, destination)
+  url <- "https://bitbucket.org/mariamedp/packrat-test-pkg/get/5a6b90280c5fec133efb88aec95014d4f9aef80f.tar.gz"
+  destination <- tempfile("packrat-test-bitbucket-", fileext = ".tar.gz")
+  result <- bitbucketDownload(url, destination)
   expect_true(result == 0)
   expect_true(file.exists(destination))
   unlink(destination)
+
 })
