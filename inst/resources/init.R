d7ad4c254d6be73814030594c1b2e0d8e23cea0a
Kevin Ushey <kevinushey@gmail.com>
2019-06-26 11:36:19 -0700

    bump version

--- inst/resources/init.R
+++ inst/resources/init.R
@@ -186,7 +186,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- "InstallAgent: packrat 0.5.0-15"
+    installAgent <- "InstallAgent: packrat 0.5.0-16"
 
     ## -- InstallSource -- ##
     installSource <- "InstallSource: source"
