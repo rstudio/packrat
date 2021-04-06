e54fd3246c53707353c31353607b2238fdf320f9
Aron Atkins <aron@gweep.net>
2021-04-06 14:05:31 -0400

    repo-empty is test-managed

--- tests/testthat/test-local-repositories.R
+++ tests/testthat/test-local-repositories.R
@@ -3,13 +3,20 @@ context("Local Repositories")
 withTestContext({
 
   test_that("init fails when package not found in any repo", {
-    projRoot <- cloneTestProject("sated")
     repos <- getOption("repos")
-    options(repos = c(CRAN = paste0("file:///", normalizePath("repo-empty"))))
+    on.exit(options(repos = repos))
+
+    emptyRepo <- file.path(tempdir(),"repo-empty")
+    on.exit(unlink(emptyRepo, recursive = TRUE))
+
+    contribDir <- file.path(emptyRepo,"src","contrib")
+    dir.create(contribDir, recursive = TRUE)
+    file.create(file.path(contribDir, "PACKAGES"))
+    projRoot <- cloneTestProject("sated")
+    options(repos = c(CRAN = paste0("file:///", emptyRepo)))
     ## we expect a warning signalling that the package 'breakfast' is not found
     ## in a repo or locally
     expect_error(suppressWarnings(init(enter = FALSE, projRoot)))
-    options(repos = repos)
   })
 
   test_that("install_local fails if no repository has been defined", {
@@ -17,7 +24,6 @@ withTestContext({
   })
 
   test_that("packrat::get_opts can read / write atrocious paths", {
-
     path <- list.files(pattern = "^Ugly")
     with_dir(tempdir(), {
       opts$local.repos(path)
@@ -25,5 +31,4 @@ withTestContext({
       expect_identical(path, readPath)
     })
   })
-
 })
