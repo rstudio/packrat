0c186705632fffc559e282f56a7ab901fae59555
Kevin Ushey <kevinushey@gmail.com>
2021-08-17 10:32:48 -0700

    add missing 'add = TRUE'

--- R/dependencies.R
+++ R/dependencies.R
@@ -623,7 +623,7 @@ fileDependencies.evaluate <- function(file) {
 
   # keep going on error
   chunkOptions <- knitr::opts_chunk$get()
-  on.exit(knitr::opts_chunk$restore(chunkOptions))
+  on.exit(knitr::opts_chunk$restore(chunkOptions), add = TRUE)
   knitr::opts_chunk$set(error = TRUE)
 
   # rudely override knitr's 'inline_exec' function so
