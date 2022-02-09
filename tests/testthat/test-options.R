070cb36a0317cb7df8734352a0fe39f3cd49d23e
Aron Atkins <aron@gweep.net>
2022-02-09 09:09:31 -0500

    update test to avoid dirtying repo & ignoring incoming state

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -1,15 +1,16 @@
-# Confirms that we can use set_opts and the single-option setter and that the
-# single-option setter does not overwrite previously configured state (#655)
-test_that("an option can be set and retrieved", {
-  initial <- get_opts()
+withTestContext({
+  # Confirms that we can use set_opts and the single-option setter and that the
+  # single-option setter does not overwrite previously configured state (#655)
+  test_that("an option can be set and retrieved", {
+    projRoot <- cloneTestProject("empty")
 
-  expected <- initial
-  expected[["ignored.packages"]] <- c("emo")
-  expected[["snapshot.recommended.packages"]] <- TRUE
+    owd <- setwd(projRoot)
+    on.exit(setwd(owd))
 
-  packrat::set_opts(ignored.packages = c("emo"), persist = FALSE)
-  packrat::opts$snapshot.recommended.packages(TRUE, persist = FALSE)
+    packrat::set_opts(ignored.packages = c("emo"), persist = FALSE)
+    packrat::opts$snapshot.recommended.packages(TRUE, persist = FALSE)
 
-  actual <- get_opts()
-  expect_equal(actual, expected)
+    expect_equal(get_opts("ignored.packages"), c("emo"))
+    expect_equal(get_opts("snapshot.recommended.packages"), TRUE)
+  })
 })
