99aab9ae16ae8d175d8dcd1072a7c74d7ae2e834
Toph Allen <toph.allen@gmail.com>
2022-09-07 13:39:13 -0400

    Add tests for bitbucketDownload and gitlabDownload

--- tests/testthat/test-bitbucket.R
+++ tests/testthat/test-bitbucket.R
@@ -18,3 +18,20 @@ test_that("bitbucketArchiveUrl returns the correct URL", {
     "https://bitbucket.org/breakfaster/museli/get/abcde12345.tar.gz"
   )
 })
+
+test_that("bitbucketDownload calls renv$download with the correct values", {
+  url <- bitbucketArchiveUrl(bitbucket_pkg_record)
+  destfile <- "/dev/null"
+
+  auth_download_option <- options(packrat.authenticated.downloads.use.renv = TRUE)
+  on.exit(options(auth_download_option), add = TRUE)
+
+  mockery::stub(bitbucketDownload, "bitbucketAuthenticated", TRUE)
+  renv_download_mock <- mockery::mock(destfile)
+  mockery::stub(bitbucketDownload, "renvDownload", renv_download_mock, depth = 3)
+
+  bitbucketDownload(url, destfile)
+
+  mockery::expect_called(renv_download_mock, 1)
+  mockery::expect_args(renv_download_mock, 1, url, destfile, type = "bitbucket")
+})
--- tests/testthat/test-gitlab.R
+++ tests/testthat/test-gitlab.R
@@ -18,3 +18,20 @@ test_that("gitlabArchiveUrl returns the correct URL", {
     "https://gitlab.com/api/v4/projects/breakfaster%2Fmuseli/repository/archive?sha=abcde12345"
   )
 })
+
+test_that("gitlabDownload calls renv$download with the correct values", {
+  url <- gitlabArchiveUrl(gitlab_pkg_record)
+  destfile <- "/dev/null"
+
+  auth_download_option <- options(packrat.authenticated.downloads.use.renv = TRUE)
+  on.exit(options(auth_download_option), add = TRUE)
+
+  mockery::stub(gitlabDownload, "gitlabAuthenticated", TRUE)
+  renv_download_mock <- mockery::mock(destfile)
+  mockery::stub(gitlabDownload, "renvDownload", renv_download_mock, depth = 3)
+
+  gitlabDownload(url, destfile)
+
+  mockery::expect_called(renv_download_mock, 1)
+  mockery::expect_args(renv_download_mock, 1, url, destfile, type = "gitlab")
+})
