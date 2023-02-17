d255e1b3848106faad631c2331b82a8fb78dfae0
Toph Allen <toph.allen@gmail.com>
2023-02-17 16:58:10 -0500

    Directly return data frames.

--- R/remote-info.R
+++ R/remote-info.R
@@ -1,32 +1,34 @@
 getRemoteInfo <- function(pkgRecord) {
   if (pkgRecord$source == "github") {
-    remote_info <- as.data.frame(
-      c(
-        list(
-          RemoteType     = pkgRecord$source,
-          GithubRepo     = pkgRecord$gh_repo,
-          GithubUsername = pkgRecord$gh_username,
-          GithubRef      = pkgRecord$gh_ref,
-          GithubSHA1     = pkgRecord$gh_sha1
-        ),
-        c(GithubSubdir   = pkgRecord$gh_subdir)
-      ),
-      stringsAsFactors = FALSE
+    return(
+      as.data.frame(
+        c(
+          list(
+            RemoteType     = pkgRecord$source,
+            GithubRepo     = pkgRecord$gh_repo,
+            GithubUsername = pkgRecord$gh_username,
+            GithubRef      = pkgRecord$gh_ref,
+            GithubSHA1     = pkgRecord$gh_sha1
+          ),
+          c(GithubSubdir   = pkgRecord$gh_subdir)
+        ), stringsAsFactors = FALSE
+      )
     )
   } else {
-    remote_info <- as.data.frame(
-    c(
-      list(
-        RemoteType     = pkgRecord$source,
-        RemoteHost     = pkgRecord$remote_host,
-        RemoteRepo     = pkgRecord$remote_repo,
-        RemoteUsername = pkgRecord$remote_username,
-        RemoteRef      = pkgRecord$remote_ref,
-        RemoteSha      = pkgRecord$remote_sha
-      ),
-      c(RemoteSubdir = pkgRecord$remote_subdir)
-    ),
-    stringsAsFactors = FALSE
+    return(
+      as.data.frame(
+        c(
+          list(
+            RemoteType     = pkgRecord$source,
+            RemoteHost     = pkgRecord$remote_host,
+            RemoteRepo     = pkgRecord$remote_repo,
+            RemoteUsername = pkgRecord$remote_username,
+            RemoteRef      = pkgRecord$remote_ref,
+            RemoteSha      = pkgRecord$remote_sha
+          ),
+          c(RemoteSubdir = pkgRecord$remote_subdir)
+        ), stringsAsFactors = FALSE
+      )
     )
   }
 }
