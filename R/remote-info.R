1ede5fe059a51ac8a25c8f1e6aad18750ebd4c26
Toph Allen <toph.allen@gmail.com>
2023-02-21 11:05:40 -0500

    simplify

--- R/remote-info.R
+++ R/remote-info.R
@@ -10,34 +10,26 @@
 getRemoteInfo <- function(pkgRecord) {
   if (pkgRecord$source == "github") {
     return(
-      as.data.frame(
-        c(
-          list(
-            RemoteType     = pkgRecord$source,
-            GithubRepo     = pkgRecord$gh_repo,
-            GithubUsername = pkgRecord$gh_username,
-            GithubRef      = pkgRecord$gh_ref,
-            GithubSHA1     = pkgRecord$gh_sha1
-          ),
-          c(GithubSubdir   = pkgRecord$gh_subdir)
-        ), stringsAsFactors = FALSE
-      )
+      as.data.frame(as.list(c(
+        RemoteType     = pkgRecord$source,
+        GithubRepo     = pkgRecord$gh_repo,
+        GithubUsername = pkgRecord$gh_username,
+        GithubRef      = pkgRecord$gh_ref,
+        GithubSHA1     = pkgRecord$gh_sha1,
+        GithubSubdir = pkgRecord$gh_subdir
+      )), stringsAsFactors = FALSE)
     )
   } else {
     return(
-      as.data.frame(
-        c(
-          list(
-            RemoteType     = pkgRecord$source,
-            RemoteHost     = pkgRecord$remote_host,
-            RemoteRepo     = pkgRecord$remote_repo,
-            RemoteUsername = pkgRecord$remote_username,
-            RemoteRef      = pkgRecord$remote_ref,
-            RemoteSha      = pkgRecord$remote_sha
-          ),
-          c(RemoteSubdir = pkgRecord$remote_subdir)
-        ), stringsAsFactors = FALSE
-      )
+      as.data.frame(as.list(c(
+        RemoteType     = pkgRecord$source,
+        RemoteHost     = pkgRecord$remote_host,
+        RemoteRepo     = pkgRecord$remote_repo,
+        RemoteUsername = pkgRecord$remote_username,
+        RemoteRef      = pkgRecord$remote_ref,
+        RemoteSha      = pkgRecord$remote_sha,
+        RemoteSubdir = pkgRecord$remote_subdir
+      )), stringsAsFactors = FALSE)
     )
   }
 }
