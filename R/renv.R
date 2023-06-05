606a4236d76b059b12547894690021ef3b9147bb
Toph Allen <toph.allen@gmail.com>
2023-06-05 17:40:30 -0400

    Update vendored renv

--- R/renv.R
+++ R/renv.R
@@ -1,7 +1,76 @@
 
-renv <- new.env()
+#
+# renv 0.17.3-62: A dependency management toolkit for R.
+# Generated using `renv:::vendor()` at 2023-06-05 17:36:24.
+#
 
-loadRenv <- function() {
-  script <- system.file("resources/renv.R", package = "packrat")
+
+renv <- new.env(parent = new.env())
+
+renv$initialize <- function() {
+
+  # set up renv + imports environments
+  attr(renv, "name") <- "embedded:renv"
+  attr(parent.env(renv), "name") <- "imports:renv"
+
+  # set up metadata environment
+  metadata <- list(
+    embedded = TRUE,
+    version  = "0.17.3-62"
+  )
+
+  # assign into renv
+  assign("_renv_metadata", as.environment(metadata), envir = renv)
+
+  # get imports
+  imports <- list(
+    utils = c(
+      "Rprof",
+      "URLencode",
+      "adist",
+      "available.packages",
+      "browseURL",
+      "citation",
+      "contrib.url",
+      "download.file",
+      "download.packages",
+      "file.edit",
+      "getCRANmirrors",
+      "head",
+      "help",
+      "install.packages",
+      "installed.packages",
+      "modifyList",
+      "old.packages",
+      "packageDescription",
+      "packageVersion",
+      "read.table",
+      "remove.packages",
+      "sessionInfo",
+      "str",
+      "summaryRprof",
+      "tail",
+      "tar",
+      "toBibtex",
+      "untar",
+      "unzip",
+      "update.packages",
+      "zip"
+    )
+  )
+
+  # load the imports required by renv
+  for (package in names(imports)) {
+    namespace <- asNamespace(package)
+    functions <- imports[[package]]
+    list2env(mget(functions, envir = namespace), envir = parent.env(renv))
+  }
+
+  # source renv into the aforementioned environment
+  script <- system.file("vendor/renv.R", package = .packageName)
   sys.source(script, envir = renv)
+
+  # remove our initialize method when we're done
+  rm(list = "initialize", envir = renv)
+
 }
